4e241d2699c412f0c6d202feec6e33b2aad65a74
keicoon15 <keicoon15@gmail.com>
2018-09-30 15:30:34 +0900

    add tinyify

--- browserify/index.js
+++ browserify/index.js
@@ -4,12 +4,11 @@ var browserify = require('browserify');
 var bundleFs = fs.createWriteStream(__dirname + '/bundle.js');
 var b = browserify();
 
-var _source_path = require('path').join(__dirname, '..', 'src').replace(/\\/g, '/'); 
+var _source_path = require('path').join(__dirname, '..', 'src').replace(/\\/g, '/');
 
 var Readable = require('stream').Readable
 var s = new Readable
 s.push(`window.AtomParser = require('${_source_path}');`);
 s.push(null)
 
-b.add(s);
-b.bundle().pipe(bundleFs);
+b.add(s).transform('envify', { global: true }).bundle().pipe(require('minify-stream')()).pipe(bundleFs)
